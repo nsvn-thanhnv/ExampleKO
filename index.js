6465005698291d1b37e42398576d920f45219c54
thanhnguyentk <thanh_nv@nittsusystem-vn.com>
2020-01-03 10:49:09 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+class CarReservation {
+    constructor(name, initialCar) {
+        this.name = ko.observable(name);
+        this.car = ko.observable(initialCar);
+
+        this.fomatPrice = ko.computed(() => {
+            var price = this.car().price;
+
+            return price ? "$" + price.toFixed(2) : "0";
+        });
+    }
+
+}
+
+class ReservationsViewModel {
+
+    availableCar = [
+        { carName: "BMW", price: 10000 },
+        { carName: "Mercerde", price: 20000 },
+        { carName: "Audi", price: 30000 }
+    ];
+
+    customer = ko.observableArray([
+        new CarReservation("Thanh", this.availableCar[0]),
+        new CarReservation("Thanh1", this.availableCar[0]),
+        new CarReservation("Thanh2", this.availableCar[0])
+    ]);
+
+    addCustomer() {
+        this.customer.push(new CarReservation("", this.availableCar[0]));
+    }
+
+    removeCustomer(car) { this.customer.remove(car) }
+
+    totalMoney = ko.computed(() => {
+        var total = 0;
+
+        for (let i = 0; i < this.customer().length; i++) {
+            total += this.customer()[i].car().price;
+        }
+
+        return total;
+    });
+
+    totalCustomer = ko.computed(() => {
+        var nameCus = "";
+
+        for (let i = 0; i < this.customer().length; i++) {
+            nameCus += this.customer()[i].name() + "; ";
+        }
+
+        return nameCus;
+    });
+}
+
+var tmp = new ReservationsViewModel();
+ko.applyBindings(tmp);
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Note:
+## Observable
+- Cần phải khai báo các thuộc tính của model bằng observable, bởi chỉ có JavaScript Object mới thông báo được cho người theo dõi biết về những thay đổi đó
+- Muốn phát hiện và phản hồi lại những thay đổi của tập hợp nhiều thứ thì dùng observableArray:
+- Một số phương thức của observableArray: 
+    - push(value): thêm mới 1 item vào cuối array
+    - pop(): lấy ra phần tử cuối cùng và trả về nó 
+    - unshift(value): chèn thêm phần tử vào đầu mảng
+    - shift(): lấy ra phần tử đầu và trả về nó
+    - reverse(), sort() 
+
+## Bindings 
+-  
